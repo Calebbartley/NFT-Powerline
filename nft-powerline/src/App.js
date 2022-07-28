185e7fdd85d409d78c7c98c13a8e374136c557bf
Caleb Bartley <cajo9@Mac-mini-7.attlocal.net>
2022-07-27 20:37:37 -0700

    service components and base issue resolved

--- nft-powerline/src/App.js
+++ nft-powerline/src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './Components/Header';
+import CollectionCard from './Components/CollectionCard';
+import {useState, useEffect} from 'react'
+import axios from 'axios';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const [powelineData, setPowerlineData] = useState([])
+
+useEffect(()=> {
+  const getMyNfts = async ()=>{
+    const openSeaData = await axios.get()
+  }
+}
+)
+
+return (
+  <div className='app'>
+    <Header/> 
+    <CollectionCard 
+    id={0} 
+    name={'ogPurp'} 
+    traits={[{'value': 7}]} 
+    image='https://lh3.googleusercontent.com/l8-sK4SSZ_hvEOrxYSBAyWukROeUny3hxbVMibbJMG9xclOc-Sntx5Q50c1hbitG897jSHViVC1Fd33nECOOcF4Zrp7WOmRhYelyxg=s0' />
+  </div>
+
+)
 }
 
 export default App;

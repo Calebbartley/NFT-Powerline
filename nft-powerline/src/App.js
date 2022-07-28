9ddf52bf5ce461d2a5000b8ab42ce09a6ac149f9
Caleb Bartley <cajo9@Mac-mini-7.attlocal.net>
2022-07-27 20:50:22 -0700

    collection address added

--- nft-powerline/src/App.js
+++ nft-powerline/src/App.js
@@ -10,18 +10,20 @@ const [powelineData, setPowerlineData] = useState([])
 
 useEffect(()=> {
   const getMyNfts = async ()=>{
-    const openSeaData = await axios.get()
+    const openseaData = await axios.get('https://testnet.opensea.io/api/v1/assets?asset_contract_address=0xB094c25AbBFb45A37F195a1BCFC25029481F6Acf')
+    console.log(openseaData.data.assets)
   }
+  return getMyNfts()
 }
 )
 
 return (
   <div className='app'>
-    <Header/> 
-    <CollectionCard 
-    id={0} 
-    name={'ogPurp'} 
-    traits={[{'value': 7}]} 
+    <Header/>
+    <CollectionCard
+    id={0}
+    name={'ogPurp'}
+    traits={[{'value': 7}]}
     image='https://lh3.googleusercontent.com/l8-sK4SSZ_hvEOrxYSBAyWukROeUny3hxbVMibbJMG9xclOc-Sntx5Q50c1hbitG897jSHViVC1Fd33nECOOcF4Zrp7WOmRhYelyxg=s0' />
   </div>
 
